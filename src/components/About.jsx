5c000e4115836b846df72e832cd241dd4f29f4ca
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-07 12:29:31 +0100

    work on about structure

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,23 @@
 import "./About.scss";
+import Dices from "../assets/images/dices_dbyd8j0oz8721dfofxhi_2.png"
 
 export default function About() {
   return (
     <section className="aboutSection">
+        <div>
+            <img src={Dices} alt="dés" />
+        </div>
+        <div>
         <h2>CONCEPT</h2>
+        <h3>Oserez-vous relever le défi ultime ? </h3>
         <p>
-        Oserez-vous relever le défi ultime ? 
-        <br /><strong>Le Jeu du Carousel</strong> est une expérience hors du commun où seuls les plus audacieux pourront prétendre à la récompense ultime : <strong>1 million d&apos;euros</strong>. 
+        <strong>Le Jeu du Carousel</strong> est une expérience hors du commun où seuls les plus audacieux pourront prétendre à la récompense ultime : <strong>1 million d&apos;euros</strong>. 
         <br />Dans ce jeu, chaque épreuve mettra à l&apos;épreuve votre intelligence, votre courage et votre capacité à survivre face aux défis les plus extrêmes. 
         <br />Ce n&apos;est pas seulement un jeu, c&apos;est une opportunité unique de changer votre vie à jamais. Mais attention, ici, chaque décision compte, et seules la détermination et la volonté de gagner vous mèneront jusqu&apos;au bout.
-        <br />Êtes-vous prêt à tout risquer pour décrocher le jackpot ? Rejoignez-nous et montrez que vous êtes fait pour le Carousel !
+        <br />Êtes-vous prêt à tout risquer pour décrocher le jackpot ? 
+        <br />Rejoignez-nous et montrez que vous êtes fait pour le Carousel !
       </p>
+      </div>
     </section>
   );
 }
