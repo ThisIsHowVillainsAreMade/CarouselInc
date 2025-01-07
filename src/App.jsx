4b66423c1062bc0eb02fb2b644e03c87d6df447f
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-07 15:49:11 +0100

    add title component over legal part

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,18 @@ import Welcome from "./components/Welcome";
 import Button from "./components/Button";
 import About from "./components/About";
 import LegalSection from "./components/LegalSection"
+import Title from "./components/Title";
 
 function App() {
   return (
     <>
       <Navbar />
       <main className="mainContent">
+        
         <Welcome />
         <Button />
         <About />
+        < Title />
         <LegalSection />
       </main>
       <Footer />
---
+++ src/components/Title.jsx
@@ -0,0 +1,72 @@
+import "./Title.scss"
+
+// inspiration sur codepen https://codepen.io/yasmo-yasmoo/pen/abgwdWZ ;)
+
+export default function Title() {
+    return(
+        <div className="container">
+  <h1 className="title">C A R O U S E L</h1>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+  <div className="line line1"></div>
+  <div className="line line2"></div>
+ 
+</div>
+    )
+}
