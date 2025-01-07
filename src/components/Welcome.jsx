5265617b0af11f92326f62166d7d6cebe098ac54
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-07 16:23:42 +0100

    fix title on welcome part

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import "./Welcome.scss";
 import GameMaster from "../assets/images/game_master_black_mask_dangerous__7cj9m50a7t2v6rvhlrd2_2.png";
 
 export default function Welcome() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const section = document.querySelector(".welcomeSection");
+    if (section) {
+      const rect = section.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom >= 0) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => setIsVisible(true), 500);
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <section className="welcomeSection">
+    <section className={`welcomeSection ${isVisible ? "visible" : "hidden"}`}>
       <h1 id="accueil">C A R O U S E L</h1>
       <h2>Osez le jeu de votre vie !</h2>
       <h3>Tentez de remporter 1 million d&apos;euros !</h3>
