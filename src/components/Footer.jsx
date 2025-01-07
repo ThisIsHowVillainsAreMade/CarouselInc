6a7bf50a7db09b660920b4a5f3c356272bee8bca
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-07 15:37:36 +0100

    fix footer date next to the copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   const getCurrentYear = () => new Date().getFullYear();
   return (
     <section className="footerSection">
-      <p>&copy; {getCurrentYear} Carousel Inc.</p>
+      <p>&copy; {getCurrentYear()} Carousel Inc.</p>
     </section>
   );
 }
