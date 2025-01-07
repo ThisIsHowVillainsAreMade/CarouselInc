0410aef0f46ee72353411829a8fc0005dbb96d18
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-07 14:10:08 +0100

    navbar adjustments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,32 @@
+// import "./Navbar.scss";
+// import logo from "../assets/images/minimalistlogo.png";
+
+// export default function Navbar() {
+//   return (
+//     <nav className="navbarSection">
+//       <section className="navbarContainer">
+//         <div className="logoSection">
+//           <a href="/" className="logo">
+//             <img src={logo} alt="Logo" />
+//           </a>
+//           <p>C A R O U S E L</p>
+//         </div>
+//         <ul className="navbarMenu">
+//           <li>
+//             <a href="#home">Accueil</a>
+//           </li>
+//           <li>
+//             <a href="#about">À propos</a>
+//           </li>
+//           <li>
+//             <a href="#contact">Mentions légales</a>
+//           </li>
+//         </ul>
+//       </section>
+//     </nav>
+//   );
+// }
+
 import "./Navbar.scss";
 import logo from "../assets/images/minimalistlogo.png";
 
@@ -7,7 +36,7 @@ export default function Navbar() {
       <section className="navbarContainer">
         <div className="logoSection">
           <a href="/" className="logo">
-            <img src={logo} alt="Logo" />
+            <img src={logo} alt="Logo" className="spinningLogo" />
           </a>
           <p>C A R O U S E L</p>
         </div>
@@ -26,3 +55,4 @@ export default function Navbar() {
     </nav>
   );
 }
+
